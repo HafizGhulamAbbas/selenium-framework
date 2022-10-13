6a5493dbc297e4ed74e47cebddfaa8fb45277bbd
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-13 10:26:59 +0500

    Added order process test cases without and with login

--- src/test/java/org/selenium/AppTest.java
+++ src/test/java/org/selenium/AppTest.java
@@ -1,36 +1,127 @@
 package org.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AppTest
 {
     @Test
-    public void dummyTest(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-
+    public void guestCheckoutUsingDirectBankTransfer(){
         WebDriverManager.chromedriver().setup();
-//        WebDriverManager.firefoxdriver().setup();
-//        WebDriverManager.iedriver().setup();
-//        WebDriverManager.edgedriver().setup();
-//        WebDriverManager.operadriver().setup();
-//        WebDriverManager.chromiumdriver().setup();
-//        WebDriverManager.safaridriver().setup();
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
-
-        // Navigate to the demoqa website
-        driver.get("https://www.demoqa.com");
-
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://askomdch.com/");
+        driver.findElement(By.cssSelector("#menu-item-1227 > a")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys("Blue");
+        driver.findElement((By.cssSelector("button[value='Search']"))).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector(".woocommerce-products-header__title.page-title")).getText(),
+                "Search results: “Blue”"
+        );
+        driver.findElement(By.cssSelector("a[aria-label='Add “Blue Shoes” to your cart']")).click();
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        driver.findElement(By.cssSelector("a[title='View cart']")).click();
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector("td[class='product-name'] a")).getText(),
+                "Blue Shoes"
+        );
+        driver.findElement(By.cssSelector(".checkout-button")).click();
+        driver.findElement(By.id("billing_first_name")).sendKeys("John");
+        driver.findElement(By.id("billing_last_name")).sendKeys("Adam");
+        driver.findElement(By.id("billing_address_1")).sendKeys("San Francisco");
+        driver.findElement(By.id("billing_city")).sendKeys("New York");
+        driver.findElement(By.id("billing_postcode")).sendKeys("94148");
+        driver.findElement(By.id("billing_email")).sendKeys("askomdch@gmail.com");
+        driver.findElement(By.id("place_order")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector(".woocommerce-notice")).getText(),
+                "Thank you. Your order has been received."
+        );
+        driver.quit();
+    }
 
+    @Test
+    public void loginAndCheckoutUsingDirectBankTransfer(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://askomdch.com/");
+        driver.findElement(By.cssSelector("#menu-item-1227 > a")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys("Blue");
+        driver.findElement((By.cssSelector("button[value='Search']"))).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector(".woocommerce-products-header__title.page-title")).getText(),
+                "Search results: “Blue”"
+        );
+        driver.findElement(By.cssSelector("a[aria-label='Add “Blue Shoes” to your cart']")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.findElement(By.cssSelector("a[title='View cart']")).click();
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector("td[class='product-name'] a")).getText(),
+                "Blue Shoes"
+        );
+        driver.findElement(By.cssSelector(".checkout-button")).click();
+        driver.findElement(By.className("showlogin")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.findElement(By.id("username")).sendKeys("demouser2");
+        driver.findElement(By.id("password")).sendKeys("demopwd");
+        driver.findElement(By.className("login")).click();
+        driver.findElement(By.id("billing_first_name")).sendKeys("John");
+        driver.findElement(By.id("billing_last_name")).sendKeys("Adam");
+        driver.findElement(By.id("billing_address_1")).sendKeys("San Francisco");
+        driver.findElement(By.id("billing_city")).sendKeys("New York");
+        driver.findElement(By.id("billing_postcode")).sendKeys("94148");
+        driver.findElement(By.id("billing_email")).clear();
+        driver.findElement(By.id("billing_email")).sendKeys("askomdch@gmail.com");
+        driver.findElement(By.id("place_order")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector(".woocommerce-notice")).getText(),
+                "Thank you. Your order has been received."
+        );
         driver.quit();
     }
 }
