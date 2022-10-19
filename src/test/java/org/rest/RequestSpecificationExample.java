5013de5281e41eab377df069356d5ff71a9ee490
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-19 19:21:40 +0500

    Request Specification: request specification builder instead of requestSpecification

--- src/test/java/org/rest/RequestSpecificationExample.java
+++ src/test/java/org/rest/RequestSpecificationExample.java
@@ -21,20 +21,26 @@ public class RequestSpecificationExample {
     @BeforeClass
     public void beforeClass() {
         // the difference between given() and with() is syntactical.
-        requestSpecification = with().
+        /*requestSpecification = with().
                 baseUri("https://api.postman.com").
                 header("x-api-key", "PMAK-634eeca9391e9e36398b8cbb-a73e5dfd984bf78ddadb401fe33e0d6773").
-                log().all();
+                log().all();*/
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri("https://api.postman.com");
+        requestSpecBuilder.addHeader("x-api-key", "PMAK-634eeca9391e9e36398b8cbb-a73e5dfd984bf78ddadb401fe33e0d6773");
+        requestSpecification = requestSpecBuilder.build();
     }
 
     @Test
     public void validate_status_code(){
-        Response response = requestSpecification.get("/workspaces");
+        // Response response = given().spec(requestSpecification).get("/workspaces").then().log().all().extract().response();
+        Response response = given(requestSpecification).get("/workspaces").then().log().all().extract().response();
         assertThat(response.statusCode(), is(equalTo(200)));
     }
     @Test
     public void validate_response_body(){
-        Response response = requestSpecification.get("/workspaces");
+        // Response response = given().spec(requestSpecification).get("/workspaces").then().log().all().extract().response();
+        Response response = given(requestSpecification).get("/workspaces").then().log().all().extract().response();
         assertThat(response.statusCode(), is(equalTo(200)));
         assertThat(response.path("workspaces[2].name").toString(), equalTo("rest-assured-framework"));
     }
