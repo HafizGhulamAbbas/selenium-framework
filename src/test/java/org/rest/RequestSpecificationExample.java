cdfd65bb01ea57a748c025da3b17f7b8b77a6d0b
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-19 19:07:23 +0500

    Request Specification: BDD to non-BBD

--- src/test/java/org/rest/RequestSpecificationExample.java
+++ src/test/java/org/rest/RequestSpecificationExample.java
@@ -23,28 +23,19 @@ public class RequestSpecificationExample {
         // the difference between given() and with() is syntactical.
         requestSpecification = with().
                 baseUri("https://api.postman.com").
-                header("x-api-key", "PMAK-634eeca9391e9e36398b8cbb-a73e5dfd984bf78ddadb401fe33e0d6773");
+                header("x-api-key", "PMAK-634eeca9391e9e36398b8cbb-a73e5dfd984bf78ddadb401fe33e0d6773").
+                log().all();
     }
 
     @Test
     public void validate_status_code(){
-        // given(requestSpecification).
-        given().spec(requestSpecification).
-        when().
-                get("/workspaces").
-        then().
-                log().all().
-                assertThat().
-                statusCode(200);
+        Response response = requestSpecification.get("/workspaces");
+        assertThat(response.statusCode(), is(equalTo(200)));
     }
     @Test
     public void validate_response_body(){
-        given().spec(requestSpecification).
-        when().
-                get("/workspaces").
-        then().
-                log().all().
-                assertThat().
-                statusCode(200);
+        Response response = requestSpecification.get("/workspaces");
+        assertThat(response.statusCode(), is(equalTo(200)));
+        assertThat(response.path("workspaces[2].name").toString(), equalTo("rest-assured-framework"));
     }
 }
