175273f858c8a68f9805059263f79e59f60385c0
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-19 12:34:49 +0500

    Automate Get Request - Assert Body

--- src/test/java/org/rest/AutomateGet.java
+++ src/test/java/org/rest/AutomateGet.java
@@ -3,6 +3,7 @@ package org.rest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class AutomateGet {
 
@@ -17,5 +18,53 @@ public class AutomateGet {
                 log().all(). // To show full response. Always use it before assertThat() function. Otherwise, it will not show response.
                 assertThat().
                 statusCode(200);
+    }
+
+    // Now let's verify the response body. We are to assert each workspaces' name, type, and visibility.
+    // We will do it using Groovy/GPath expression
+    // Response object is below
+    /* {
+        "workspaces": [
+        {
+            "id": "80c02df0-4832-4484-8d3d-6a64d598010a",
+                "name": "My Workspace",
+                "type": "personal",
+                "visibility": "only-me"
+        },
+        {
+            "id": "8f9fba5a-bc93-469e-8403-ef9e4a2f606c",
+                "name": "Team Workspace",
+                "type": "team",
+                "visibility": "team"
+        },
+        {
+            "id": "15ee1f23-06f2-40c1-a85c-2f314f0e7d79",
+                "name": "rest-assured-framework",
+                "type": "team",
+                "visibility": "team"
+        }
+    ]
+    }*/
+    @Test
+    public void validate_response_body(){
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", "PMAK-634eeca9391e9e36398b8cbb-a73e5dfd984bf78ddadb401fe33e0d6773").
+                when().
+                get("/workspaces").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name", hasItems("Team Workspace", "My Workspace", "rest-assured-framework"),
+                        "workspaces.type", hasItems("team", "personal", "personal"),
+                        "workspaces[0].name", equalTo("My Workspace"),
+                        "workspaces[1].name", is(equalTo("Team Workspace")),
+                        "workspaces[2].name", is(equalTo("rest-assured-framework")),
+                        "workspaces[0].visibility", equalTo("only-me"),
+                        "workspaces[1].visibility", is(equalTo("team")),
+                        "workspaces[2].visibility", is(equalTo("team")),
+                        "workspaces.size()", equalTo(3)
+                );
     }
 }
