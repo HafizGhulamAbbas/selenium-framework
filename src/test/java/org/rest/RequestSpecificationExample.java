5044f35589fd5d27dadb44d00bf26c580ceeabfc
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-19 18:58:13 +0500

    Request Specification: reuse request specification

--- src/test/java/org/rest/RequestSpecificationExample.java
+++ src/test/java/org/rest/RequestSpecificationExample.java
@@ -10,20 +10,24 @@ import io.restassured.specification.SpecificationQuerier;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class RequestSpecificationExample {
+    RequestSpecification requestSpecification;
+
+    @BeforeClass
+    public void beforeClass() {
+        // the difference between given() and with() is syntactical.
+        requestSpecification = with().
+                baseUri("https://api.postman.com").
+                header("x-api-key", "PMAK-634eeca9391e9e36398b8cbb-a73e5dfd984bf78ddadb401fe33e0d6773");
+    }
 
     @Test
     public void validate_status_code(){
-        RequestSpecification requestSpecification = given().
-                baseUri("https://api.postman.com").
-                header("x-api-key", "PMAK-634eeca9391e9e36398b8cbb-a73e5dfd984bf78ddadb401fe33e0d6773");
-
         // given(requestSpecification).
         given().spec(requestSpecification).
         when().
@@ -32,6 +36,15 @@ public class RequestSpecificationExample {
                 log().all().
                 assertThat().
                 statusCode(200);
-
+    }
+    @Test
+    public void validate_response_body(){
+        given().spec(requestSpecification).
+        when().
+                get("/workspaces").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200);
     }
 }
