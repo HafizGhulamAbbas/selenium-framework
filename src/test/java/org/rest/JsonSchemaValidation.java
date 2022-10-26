ac75fefa8b07ff89c3bc180e1c928db224927d33
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-26 13:11:50 +0500

    JSON Schema Validation

---
+++ src/test/java/org/rest/JsonSchemaValidation.java
@@ -0,0 +1,24 @@
+package org.rest;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class JsonSchemaValidation {
+
+    @Test
+    public void validateJsonSchema() {
+        // https://www.jsonschema.net/app/schemas/0 - Json Schema Online
+        given().
+                baseUri("https://postman-echo.com").
+                log().all().
+        when().
+                get("/get").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                body(matchesJsonSchemaInClasspath("EchoGet.json"));
+    }
+}
