d8fcbe78feb0f64b0752224623968e90a5deb9a9
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-26 11:27:06 +0500

    Request Parameter: Multiple Query Parameter

--- src/test/java/org/rest/RequestParameters.java
+++ src/test/java/org/rest/RequestParameters.java
@@ -26,4 +26,24 @@ public class RequestParameters {
                 assertThat().
                 statusCode(200);
     }
+
+    @Test
+    public void multiple_query_parameters(){
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("foo1", "bar1");
+        queryParams.put("foo2", "bar2");
+        given().
+                baseUri("https://postman-echo.com").
+                // param("foo3", "bar3"). // Request Params
+                // queryParam("foo1", "bar1").
+                // queryParam("foo2", "bar2").
+                queryParams(queryParams). // Query Params
+                log().all().
+        when().
+                get("/get").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
 }
