bcb597a8e59ce1c2d07934cb75c543fe5626dbe2
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-11-11 11:28:50 +0500

    Page Factory Implementation: only for one class

--- src/test/java/org/selenium/pom/pages/CartPage.java
+++ src/test/java/org/selenium/pom/pages/CartPage.java
@@ -2,17 +2,24 @@ package org.selenium.pom.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.selenium.pom.base.BasePage;
 
 public class CartPage extends BasePage {
-    private final By productName = By.cssSelector("td[class='product-name'] a");
-    private final By checkoutButton = By.cssSelector(".checkout-button");
+    /*private final By productName = By.cssSelector("td[class='product-name'] a");
+    private final By checkoutButton = By.cssSelector(".checkout-button");*/
+    @FindBy(css = "td[class='product-name'] a") private WebElement productName;
+    @FindBy(how = How.CSS, using = ".checkout-button") private WebElement checkoutButton;
     public CartPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this); // This can be used in Base class. But we are not implementing it on the whole project.
     }
     public String getProductName() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(productName)).getText();
+        return wait.until(ExpectedConditions.visibilityOf(productName)).getText();
     }
     public CheckoutPage checkout() {
         wait.until(ExpectedConditions.elementToBeClickable(checkoutButton)).click();
